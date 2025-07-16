a3b76eafacee17309d3dc525415d4cf7cec88455
BlueFyre22 <bluefyr22@gmail.com>
2025-07-16 13:27:45 -0600

    added our colors, changed photos in dbcontext and service

--- server/src/services/PhotosService.js
+++ server/src/services/PhotosService.js
@@ -2,7 +2,7 @@ import { dbContext } from "../db/DbContext.js"
 
 class PhotosService {
   async createPhoto(photoData) {
-    const photo = await dbContext.Photo.create(photoData)
+    const photo = await dbContext.Photos.create(photoData)
     await photo.populate('creator')
     return photo
   }
