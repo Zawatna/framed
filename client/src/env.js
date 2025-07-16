03f5222148befce2e79e1f8bf84ec70e82cc0fad
BlueFyre22 <bluefyr22@gmail.com>
2025-07-16 10:41:44 -0600

    my env files are good now

--- client/src/env.js
+++ client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-yanh5or5lk0h4yvz.us.auth0.com'
+export const clientId = 'T8vuCvLjhGdBjmjFiAMpBPXtsYROhSqK'
+export const audience = 'https://gregoryslist.com'
