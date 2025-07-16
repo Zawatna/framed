30126dec88507b4bca7e608555eea37f9968403c
Anthony Rossman <anthony.rossman0825@gmail.com>
2025-07-16 13:58:37 -0600

    Get All Photos

--- client/src/services/PhotosService.js
+++ client/src/services/PhotosService.js
@@ -1,11 +1,21 @@
 import { logger } from "@/utils/Logger.js"
 import { api } from "./AxiosService.js"
+import { Photo } from "@/models/Photo.js"
+import { AppState } from "@/AppState.js"
 
 class PhotosService {
+  
   async createPhoto(formData) {
     const response = await api.post(`api/photos`, formData)
     logger.log("Did it work?♦️💖🗺️", response.data)
     return response.data
+  }
+
+  async getAllPhotos() {
+    const res = await api.get('api/photos');
+    logger.log('All Photos:', res.data);
+    const photos = res.data.map((pojo) => new Photo(pojo));
+    AppState.photos = photos;
   }
 
 
--- server/src/services/PhotosService.js
+++ server/src/services/PhotosService.js
@@ -7,6 +7,7 @@ class PhotosService {
     return photo
   }
 
+  
 
 }
 
