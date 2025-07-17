8d450aa6e446dd29cfbcaded593d541609adcc24
Anthony Rossman <anthony.rossman0825@gmail.com>
2025-07-17 11:08:38 -0600

    Footer button works for create

--- server/src/models/Photo.js
+++ server/src/models/Photo.js
@@ -5,7 +5,7 @@ export const PhotoSchema = new Schema(
     creatorId: { type: Schema.ObjectId, required: true, ref: "Account" },
     name: { type: String, required: true, minLength: 3, maxLength: 50 },
     imgUrl: { type: String, required: true, maxLength: 500 },
-    description: { type: String, minLength: 15, maxLength: 1000 },
+    description: { type: String, maxLength: 1000 },
     originalDate: { type: Date, required: false },
     likes: { type: Schema.ObjectId, required: false },
     location: { type: String, required: false, minLength: 1, maxLength: 500 },
