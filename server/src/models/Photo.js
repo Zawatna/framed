94bf1cbf2454f4db430f59b31299dd809d1989ff
BlueFyre22 <bluefyr22@gmail.com>
2025-07-17 12:07:06 -0600

    updated model server side

--- server/src/models/Photo.js
+++ server/src/models/Photo.js
@@ -5,10 +5,10 @@ export const PhotoSchema = new Schema(
     creatorId: { type: Schema.ObjectId, required: true, ref: "Account" },
     name: { type: String, required: true, minLength: 3, maxLength: 50 },
     imgUrl: { type: String, required: true, maxLength: 500 },
-    description: { type: String, maxLength: 1000 },
+    description: { type: String, maxLength: 100 },
     originalDate: { type: Date, required: false },
     likes: { type: Schema.ObjectId, required: false },
-    location: { type: String, required: false, minLength: 1, maxLength: 500 },
+    location: { type: String, required: false, maxLength: 500 },
   },
   {
     timestamps: true,
