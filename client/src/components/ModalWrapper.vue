288f74e4ec780bdcd20177d65fe2f2e6a63071a1
Anthony Rossman <anthony.rossman0825@gmail.com>
2025-07-17 10:22:38 -0600

    Page Overscroll X & Y are gone now/ Reset Modal

--- client/src/components/ModalWrapper.vue
+++ client/src/components/ModalWrapper.vue
@@ -8,14 +8,18 @@
 </script>
 <template>
   <div class="modal fade bg-primary text-secondary" :id="modalId" tabindex="-1" :aria-labelledby="`${modalId}Label`" aria-hidden="true">
-    <div class="modal-header">
-      <h1 class="modal-title fs-5 fancy-font" :id="`${modalId}Label`">
-        {{ modalHeader }}
-      </h1>
-      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-    </div>
-    <div class="modal-body">
-      <slot></slot>
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5 fancy-font" :id="`${modalId}Label`">
+            {{ modalHeader }}
+          </h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <slot></slot>
+        </div>
+      </div>
     </div>
   </div>
 </template>
