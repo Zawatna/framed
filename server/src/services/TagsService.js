389b9c5bdbaf99a02ba2e8645503dd2af0d373af
BlueFyre22 <bluefyr22@gmail.com>
2025-07-18 11:55:49 -0600

    server - photo tags now attach to the photo

--- server/src/services/TagsService.js
+++ server/src/services/TagsService.js
@@ -2,7 +2,16 @@ import { dbContext } from "../db/DbContext.js"
 
 class TagsService {
     async checkForNewTags(tags) {
-        const currentTags = dbContext.Tags.find()
+        const createPhotoTags = await dbContext.Tags.find({ name: tags }) // get tags that are created already
+        const tagNames = createPhotoTags.map(tag => tag.name) // turn tags that are created into strings
+        const createNewTags = tags.filter(tag => !tagNames.includes(tag)) // compare tags that are created vs ones that arent and store uncreated ones 
+        const newTagsData = createNewTags.map(str => { return { name: str } }) // turn the strings of uncreated Tags into objects 
+
+        const newTagsCreated = await this.createTag(newTagsData) // create the new tags
+        //ignore this error, it works...probably - Jeremy
+        createPhotoTags.push(...newTagsCreated) // add the newly created tags to the already created tags array
+
+        return createPhotoTags //send back an array of all tag strings they sent us now as tags
     }
     async deleteTag(tagId) {
         const deletedTag = await dbContext.Tags.findById(tagId)
