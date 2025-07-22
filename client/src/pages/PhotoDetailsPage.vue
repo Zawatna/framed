c15bb77f871407940b5fab253e6392168180bb9a
Zawatna <canamerican@hotmail.com>
2025-07-22 11:48:39 -0600

    modal moved to navbar

--- client/src/pages/PhotoDetailsPage.vue
+++ client/src/pages/PhotoDetailsPage.vue
@@ -64,7 +64,11 @@ async function getPhotoById() {
           <h4 class="img-desc main-font">{{ photo.description }}</h4>
           <div class="row align-items-center">
             <div class="col-6 d-flex ps-4">
-              <i class="mdi mdi-comment display-3 text-light"></i>
+              <button class="btn btn-success fixed-button m-2" type="button" data-bs-toggle="modal"
+                data-bs-target="#commentModal">
+                ????
+                <i class="mdi mdi-comment display-3 text-light"></i>
+              </button>
               <p class="mt-2 ms-1 fs-4">33</p>
             </div>
             <div class="col-6 text-end justify-content-end d-flex pe-4">
@@ -92,39 +96,6 @@ async function getPhotoById() {
     </div>
     <!-- TODO CREATE V-FOR ONCE PROFILE/PROFILE IMAGES ROUTE CREATED -->
 
-    <div class="row justify-content-center mb-5">
-      <div class="frame justify-content-start mt-3 pb-1 px-1">
-        <RouterLink :to="{ name: 'Photo Details', params: { photoId: photo.id } }">
-          <div class="">
-            <img :src="photo.imgUrl" :alt="`@${photo.creator.name}'s posted photo`" class="img-fluid" />
-          </div>
-        </RouterLink>
-        <div v-if="photo.creatorId != account?.id">
-          <RouterLink :to="{ name: 'Profile', params: { profileId: photo?.creatorId } }">
-            <h5 class="img-username">@{{ photo.creator.name }}</h5>
-          </RouterLink>
-        </div>
-        <div v-else>
-          <RouterLink :to="{ name: 'Profile', params: { profileId: photo?.creatorId } }">
-            <h5 class="img-username">@{{ photo.creator.name }}</h5>
-          </RouterLink>
-        </div>
-        <h4 class="img-desc main-font">{{ photo.description }}</h4>
-        <div class="row align-items-center">
-          <div class="col-6 d-flex ps-4">
-            <i class="mdi mdi-comment display-3 text-light"></i>
-            <p class="mt-2 ms-1 fs-4">33</p>
-          </div>
-          <div class="col-6 text-end justify-content-end d-flex pe-4">
-            <p class="like-num me-1 fs-4">1.3K</p>
-            <i class="mdi mdi-heart text-warning display-3" role="button"></i>
-          </div>
-        </div>
-      </div>
-    </div>
-    <ModalWrapper modalId="commentModal" modalHeader="comments go here">
-      what's up
-    </ModalWrapper>
   </div>
   <div v-else class="mt-5 container text-warning">
     <div class="row text-center">
