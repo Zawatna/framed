3943b3e524da3e244e682adb2fb36a65724c3764
Anthony Rossman <anthony.rossman0825@gmail.com>
2025-07-24 11:59:48 -0600

    broken photo details page

--- client/src/pages/PhotoDetailsPage.vue
+++ client/src/pages/PhotoDetailsPage.vue
@@ -89,122 +89,162 @@ function toggleView() {
 </script>
 
 <template>
-  <div v-if="photo && !view" class="container-fluid mb-5 main-font">
-    <div class="row justify-content-center mb-5">
-      <div class="row justify-content-center mb-5">
-        <div class="frame justify-content-start mt-3 pb-1 px-1">
+  <div>
+    <div v-if="photo" class="container-fluid mb-5 main-font flip-box">
+      <div class="row justify-content-center mb-5 flip-box-inner">
+        <div class="row justify-content-center mb-5 flip-box-front">
+          <div class="frame justify-content-start mt-3 pb-1 px-1">
 
-          <div class="text-center">
-            <img @click="toggleView()" :src="photo.imgUrl" :alt="`@${photo.creator.name}'s posted photo`"
-              class="img-fluid" />
-          </div>
+            <div class="text-center">
+              <img :src="photo.imgUrl" :alt="`@${photo.creator.name}'s posted photo`"
+                class="img-fluid" />
+            </div>
 
-          <div>
-            <RouterLink :to="{ name: 'Profile', params: { profileId: photo?.creatorId } }">
-              <h5 class="img-username">@{{ photo.creator.name }}</h5>
-            </RouterLink>
-          </div>
+            <div>
+              <RouterLink :to="{ name: 'Profile', params: { profileId: photo?.creatorId } }">
+                <h5 class="img-username">@{{ photo.creator.name }}</h5>
+              </RouterLink>
+            </div>
 
-          <h4 class="img-desc main-font">{{ photo.description }}</h4>
-          <div class="row align-items-center">
-            <div class="col-12 d-flex ps-4 align-items-center justify-content-between">
-              <div>
-                <button class="btn fixed-button m-2" type="button" data-bs-toggle="modal"
-                  data-bs-target="#commentModal">
-                  <i class="mdi mdi-comment mdi-flip-h fs-1 text-light text-shadow"></i>
-                </button>
-                <span class="mt-2 ms-1 fs-3">{{ photoComments.length }}</span>
-              </div>
-              <div class="d-flex align-items-center">
-                <p class="like-num me-1 fs-4">{{ photo.likes.length }}</p>
-                <button @click="likePhoto()" class="btn fs-1 text-warning text-shadow">
-                  <i v-if="photo.likes.find((id) => id == account?.id)" class="mdi mdi-heart"></i>
-                  <i v-else class="mdi mdi-heart-outline"></i>
-                </button>
+            <h4 class="img-desc main-font">{{ photo.description }}</h4>
+            <div class="row align-items-center">
+              <div class="col-12 d-flex ps-4 align-items-center justify-content-between">
+                <div>
+                  <button class="btn fixed-button m-2" type="button" data-bs-toggle="modal"
+                    data-bs-target="#commentModal">
+                    <i class="mdi mdi-comment mdi-flip-h fs-1 text-light text-shadow"></i>
+                  </button>
+                  <span class="mt-2 ms-1 fs-3">{{ photoComments.length }}</span>
+                </div>
+                <div class="d-flex align-items-center">
+                  <p class="like-num me-1 fs-4">{{ photo.likes.length }}</p>
+                  <button @click="likePhoto()" class="btn fs-1 text-warning text-shadow">
+                    <i v-if="photo.likes.find((id) => id == account?.id)" class="mdi mdi-heart"></i>
+                    <i v-else class="mdi mdi-heart-outline"></i>
+                  </button>
+                </div>
               </div>
             </div>
+          </div>
+        </div>
+        <div class="row justify-content-center mb-5 flip-box-back">
+
+        <div class="frame mt-3 pb-1 px-1">
+          <div class="row">
+            <div class="col-5 mt-5 ms-2">
+              <h1>Location: </h1>
+              <h3>{{ photo.location }}</h3>
+            </div>
+            <div class="col-6 mt-2 ms-4">
+              <h5 class="mt-5 mb-4 fs-1">{{ photo.description }}</h5>
+              <hr class="text-primary">
+              <h1 class="mb-4">Tags:</h1>
+              <div class="d-flex flex-wrap justify-content-center align-items-center">
+                <div v-for="tag in photo.tags" :key="tag.id"
+                  class="badge border rounded bg-success text-light fs-3 px-1 me-2 ms-2 mb-2">{{ tag.tag.name }}</div>
+              </div>
+            </div>
+          </div>
+          <div class="row mt-3">
+            <div class="col-12 mt-5">
+              <h2>Taken On: {{ photo.originalDate.toLocaleDateString('en-US', {
+                weekday: 'long', year: 'numeric', month:
+                  'long', day: 'numeric' }) }}</h2>
+              <h2>Uploaded On: {{ photo.uploadedAt.toLocaleDateString('en-US', {
+                weekday: 'long', year: 'numeric', month:
+                  'long', day: 'numeric' }) }}</h2>
+            </div>
+          </div>
+          <div class="row text-end mt-5 me-3">
+            <h4 class="mt-5">Photographed by: <span>
+                <RouterLink :to="{ name: 'Profile', params: { profileId: photo.creatorId } }">@{{ photo.creator.name }}
+                </RouterLink>
+              </span></h4>
           </div>
         </div>
       </div>
     </div>
     <div class="ms-5 display-inline d-flex gap-4">
       <AlbumPhotoButton v-if="account" />
-      <div>
-        <button v-if="photo.creatorId == account?.id" @click="deletePhoto()" type="button"
-          class="btn btn-danger text-light text-shadow">
-          Delete this photo
-        </button>
+        <div>
+          <button v-if="photo.creatorId == account?.id" @click="deletePhoto()" type="button"
+            class="btn btn-danger text-light text-shadow">
+            Delete this photo
+          </button>
+        </div>
       </div>
-    </div>
-    <div class="row text-center bg-primary">
-      <h1 v-if="photo.creatorId != account?.id" class="col-12 text-light mt-3 mb-3">
-        More Photos from
-        <RouterLink :to="{ name: 'Profile', params: { profileId: photo.creatorId } }">
-          <span class="username">@{{ photo.creator.name }}</span>
-        </RouterLink>
-      </h1>
-      <h1 v-else class="col-12 text-light mt-3 mb-3">
-        More Photos from
-        <RouterLink :to="{ name: 'Account' }">
-          <span class="username">@{{ photo.creator.name }}</span>
-        </RouterLink>
-      </h1>
-    </div>
-    <!-- TODO CREATE V-FOR ONCE PROFILE/PROFILE IMAGES ROUTE CREATED -->
-  </div>
-  <div v-else-if="photo && view" class="container-fluid mb-5 fancy-font">
-    <div class="row justify-content-center mb-5">
-
-      <div class="frame mt-3 pb-1 px-1">
-        <div class="row">
-          <div class="col-5 mt-5 ms-2">
-            <h1>Location: </h1>
-            <h3>{{ photo.location }}</h3>
-          </div>
-          <div class="col-6 mt-2 ms-4">
-            <h5 class="mt-5 mb-4 fs-1">{{ photo.description }}</h5>
-            <hr class="text-primary">
-            <h1 class="mb-4">Tags:</h1>
-            <div class="d-flex flex-wrap justify-content-center align-items-center">
-              <div v-for="tag in photo.tags" :key="tag.id"
-                class="badge border rounded bg-success text-light fs-3 px-1 me-2 ms-2 mb-2">{{ tag.tag.name }}</div>
-            </div>
-          </div>
-        </div>
-        <div class="row mt-3">
-          <div class="col-12 mt-5">
-            <h2>Taken On: {{ photo.originalDate.toLocaleDateString('en-US', {
-              weekday: 'long', year: 'numeric', month:
-                'long', day: 'numeric' }) }}</h2>
-            <h2>Uploaded On: {{ photo.uploadedAt.toLocaleDateString('en-US', {
-              weekday: 'long', year: 'numeric', month:
-                'long', day: 'numeric' }) }}</h2>
-          </div>
-        </div>
-        <div class="row text-end mt-5 me-3">
-          <h4 class="mt-5">Photographed by: <span>
-              <RouterLink :to="{ name: 'Profile', params: { profileId: photo.creatorId } }">@{{ photo.creator.name }}
-              </RouterLink>
-            </span></h4>
-        </div>
-
-
+      <div class="row text-center bg-primary">
+        <h1 v-if="photo.creatorId != account?.id" class="col-12 text-light mt-3 mb-3">
+          More Photos from
+          <RouterLink :to="{ name: 'Profile', params: { profileId: photo.creatorId } }">
+            <span class="username">@{{ photo.creator.name }}</span>
+          </RouterLink>
+        </h1>
+        <h1 v-else class="col-12 text-light mt-3 mb-3">
+          More Photos from
+          <RouterLink :to="{ name: 'Account' }">
+            <span class="username">@{{ photo.creator.name }}</span>
+          </RouterLink>
+        </h1>
       </div>
+      <!-- TODO CREATE V-FOR ONCE PROFILE/PROFILE IMAGES ROUTE CREATED -->
     </div>
-
-  </div>
-  <div v-else class="mt-5 container text-warning main-font">
-    <div class="row text-center">
-      <div class="col-12 display-1">
-        Loading
-        <i class="mdi mdi-fan mdi-spin display-1"></i>
-        ...
+    <div v-else class="mt-5 container text-warning main-font">
+      <div class="row text-center">
+        <div class="col-12 display-1">
+          Loading
+          <i class="mdi mdi-fan mdi-spin display-1"></i>
+          ...
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
+
+
+.flip-box {
+  background-color: transparent;
+  width: 525px;
+  height: 200px;
+
+  perspective: 1000px;
+}
+
+.flip-box-inner {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  transition: transform 0.8s;
+  transform-style: preserve-3d;
+}
+
+.flip-box:hover .flip-box-inner {
+  transform: rotateY(180deg);
+}
+
+.flip-box-front, .flip-box-back {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  -webkit-backface-visibility: hidden;
+  backface-visibility: hidden;
+}
+
+.flip-box-front {
+
+  color: black;
+}
+
+.flip-box-back {
+
+  color: black;
+  transform: rotateY(180deg);
+}
+
+
 a {
   text-decoration: none;
   color: black;
